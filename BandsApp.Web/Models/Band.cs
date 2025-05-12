802a066384a3d46db420206a28fe763048036913
KakanDrakan <tobias.lindskog@yahoo.com>
2025-05-12 16:58:39 +0200

    the rest i suppose

--- BandsApp.Web/Models/Band.cs
+++ BandsApp.Web/Models/Band.cs
@@ -5,5 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; } = null!;
         public string Description { get; set; } = null!;  
+        public string Image { get; set; } = null!;
+        public string VideoUrl { get; set; } = null!;
     }
 }
--- BandsApp.Web/Program.cs
+++ BandsApp.Web/Program.cs
@@ -2,15 +2,17 @@ namespace BandsApp.Web
 {
     public class Program
     {
-        // Kommentar här
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddControllersWithViews();
             var app = builder.Build();
             app.MapControllers();
+            
+            app.MapStaticAssets();
 
-            app.Run(); //comment
+            app.Run();
+            
         }
     }
 }
