aed7765e17ab20eb6f0199b90d455ecbaad377f0
Jörgen Hägglund <jorgen@sirlucifer.mine.nu>
2025-05-12 16:20:45 +0200

    Most structural things fixed.

---
+++ BandsApp.Web/Controllers/BandsController.cs
@@ -0,0 +1,27 @@
+﻿using BandsApp.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BandsApp.Web.Controllers;
+
+public class BandsController : Controller
+{
+    BandService bandService = new();
+
+    [Route("")]
+    public IActionResult Index()
+    {
+        var bands = bandService.GetAllBands();
+        return View(bands);
+    }
+
+    [Route("details/{id:int}")]
+    public IActionResult Details(int id)
+    {
+        var band = bandService.GetBandById(id);
+        if (band == null)
+        {
+            return NotFound();
+        }
+        return View(band);
+    }
+}
---
+++ BandsApp.Web/Services/BandService.cs
@@ -0,0 +1,20 @@
+﻿using BandsApp.Web.Models;
+
+namespace BandsApp.Web.Services;
+
+public class BandService
+{
+    List<Band> bands = new List<Band>
+    {
+        new Band { Id = 1, Name = "Jorn", Description = "Description A" },
+        new Band { Id = 2, Name = "Iron Maiden", Description = "Description B" },
+        new Band { Id = 3, Name = "Amazarashi", Description = "Description C" }
+    };
+
+    public Band[] GetAllBands() =>
+        bands.OrderBy(b => b.Name)
+        .ToArray();
+
+    public Band? GetBandById(int id) =>
+        bands.SingleOrDefault(b => b.Id == id);
+}
